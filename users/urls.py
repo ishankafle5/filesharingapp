eb47a422e107fb0e3cd30259265eb0bbde49d5a2
Ishan Kafle <www.2020ishankafle@gmail.com>
2023-06-25 08:10:32 +0545

    halfofcss complete

---
+++ users/urls.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from django.urls import path
+from users import views
+urlpatterns = [
+    path('register', views.signup_page), path('login', views.login_page),
+]
